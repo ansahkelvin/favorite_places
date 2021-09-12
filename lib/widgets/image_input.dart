abda8da6673eb43913d12cb4b27ba36b9a824884
ansahkelvin <ansahkelvin0@gmail.com>
2021-09-12 15:59:20 +0000

    Added the image picker

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -12,6 +13,20 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? storedImage;
+
+  Future<void> pickImage() async {
+    try {
+      final imagePicked =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+      setState(() {
+        storedImage = File(imagePicked!.path);
+      });
+    } catch (e) {
+      // ignore: avoid_print
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -27,20 +42,22 @@ class _ImageInputState extends State<ImageInput> {
             decoration: BoxDecoration(
               color: Colors.grey[200],
             ),
-            child: Center(
-              child: storedImage == null
-                  ? const Text("No Image Seleted")
-                  : Image.file(storedImage!, fit: BoxFit.cover,),
-            ),
+            child: storedImage == null
+                ? const Center(child: Text("No Image Seleted"))
+                : Image.file(
+                    storedImage!,
+                    fit: BoxFit.cover,
+                  ),
           ),
+          const SizedBox(width: 10),
           Expanded(
             child: TextButton.icon(
-              onPressed: () {},
+              onPressed: pickImage,
               style: TextButton.styleFrom(
                 primary: Colors.black,
               ),
               icon: const Icon(CupertinoIcons.camera),
-              label: const Text("Select Image"),
+              label: const Text("Take Picture"),
             ),
           ),
         ],
